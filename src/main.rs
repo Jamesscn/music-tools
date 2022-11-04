18f2cac3230552ae158a82c32d9a6bfb94736c18
James Scoon <jamesscn@gmail.com>
2022-11-04 00:16:31 -0600

    Try multiple sounds at the same time

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 pub mod note;
 pub mod audio;
+pub mod scale;
 pub mod pitchclass;
 pub use std::time::Duration;
-pub use rodio::OutputStream;
+pub use rodio::{OutputStream, OutputStreamHandle, Sink};
 pub use crate::note::Note;
 pub use crate::audio::{WavetableOscillator, Source};
 
+pub use crate::scale::get_scale;
+pub use crate::pitchclass::get_pitch_class;
+
+fn _play_note(note: &'static str, stream_handle_ref: &OutputStreamHandle, sinks: &mut Vec<Sink>) {
+    let current_note = Note::new(&note);
+    let oscillator = WavetableOscillator::new(128, current_note.frequency, 44100);
+    let sink = Sink::try_new(stream_handle_ref).unwrap();
+    sink.append(oscillator);
+    sinks.push(sink);
+}
+
 fn main() {
-    let notes = ["A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5"];
-    for note in notes {
-        let current_note = Note::new(note);
-        let oscillator = WavetableOscillator::new(64, current_note.frequency, 44100);
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        let _result = stream_handle.play_raw(oscillator.convert_samples());
-        std::thread::sleep(Duration::from_secs(1));
-    }
+    let mut sinks: Vec<Sink> = Vec::new();
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    play_note("C4", &stream_handle, &mut sinks);
+    play_note("E4", &stream_handle, &mut sinks);
+    play_note("G4", &stream_handle, &mut sinks);
+    std::thread::sleep(Duration::from_secs(3));
 }
