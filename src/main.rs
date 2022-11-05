a5ac0a2eea790c36f5726193fe176ca014aa9a5a
James Scoon <jamesscn@gmail.com>
2022-11-05 13:07:29 -0600

    Add chord progression example

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,12 @@ pub use rodio::{OutputStream, OutputStreamHandle, Sink};
 pub use audio::{WavetableOscillator, Source};
 
 pub use chord::get_chord_with_quality;
-pub use pitchclass::PitchClasses;
+pub use pitchclass::{PitchClass, PitchClasses};
 pub use enums::{ChordQuality, Seventh};
 pub use note::Note;
+pub use scale::{get_scale, ScaleType};
+
+use crate::chord::get_chord_from_numeral;
 
 fn play_note(note: &Note, stream_handle_ref: &OutputStreamHandle, sinks: &mut Vec<Sink>) {
     let oscillator = WavetableOscillator::new(128, note.frequency, 44100);
@@ -21,16 +24,44 @@ fn play_note(note: &Note, stream_handle_ref: &OutputStreamHandle, sinks: &mut Ve
     sinks.push(sink);
 }
 
+fn get_notes_with_octave(pitch_classes: Vec<&'static PitchClass>, starting_octave: u8) -> Vec<Note> {
+    let mut notes: Vec<Note> = Vec::new();
+    let mut octave: u8 = starting_octave;
+    let mut last_value: i8 = -1;
+    for pitch_class in pitch_classes {
+        let pitch_class_value = pitch_class.get_value();
+        if pitch_class_value as i8 <= last_value {
+            octave += 1;
+        }
+        let note = Note::from(pitch_class, octave);
+        notes.push(note);
+        last_value = pitch_class_value as i8;
+    }
+    return notes;
+}
+
 fn main() {
-    let tonic = PitchClasses::D_FLAT;
-    let chord = get_chord_with_quality(tonic, ChordQuality::Minor, Seventh::Major, 1);
-    let chord_pitch_classes = chord.get_pitch_classes();
-    println!("Playing {0}", chord.get_short_name());
-    for pitch_class in chord_pitch_classes {
-        let mut sinks: Vec<Sink> = Vec::new();
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        let note = Note::from(pitch_class, 4);
-        play_note(&note, &stream_handle, &mut sinks);
-        std::thread::sleep(Duration::from_secs(1));
+    let tonic = PitchClasses::C;
+    let scale = get_scale(tonic, ScaleType::Major, scale::Pentatonic::None);
+    let progression = ["IV", "V", "iii", "vi", "I", "bVI", "bVII", "I"];
+    let octaves = [4, 4, 4, 4, 4, 4, 4, 5];
+    for (index, numeral) in progression.iter().enumerate() {
+        let chord = get_chord_from_numeral(&scale, numeral);
+        let octave = octaves[index];
+        println!("Playing {}{}", chord.get_short_name(), octave);
+        let mut notes_played = 0;
+        'measure: loop {
+            let notes = get_notes_with_octave(chord.get_pitch_classes(), octave);
+            for note in notes {
+                let mut sinks: Vec<Sink> = Vec::new();
+                let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+                play_note(&note, &stream_handle, &mut sinks);
+                std::thread::sleep(Duration::from_millis(150));
+                notes_played += 1;
+                if notes_played == 8 {
+                    break 'measure;
+                }
+            }
+        }
     }
 }
